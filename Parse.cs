7e85e17ff37dbe4c36f7468e8c80eae77eca8a13
vmaricru <danila.sergeev2010@yandex.ru>
2022-04-22 18:40:45 +0300

    21.04.2022

---
+++ Parse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChemicalHierarchy;
+
+namespace ChemicalParser
+{
+    class Parse
+    {
+        private string allowedSym = "1234567890-<>()[]-.ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz/";
+        private string InputStr;
+        private Dictionary<ChemicalElement, ChemicalQuantity> ChemList; //список элементов с их характеристикой
+
+        //private struct elemQQ //Качество и кол-во элемента
+       // {
+         //   public string elemName;
+           // public ChemicalQuantity ChemicalQuantity ;
+      //  }
+
+        public Parse(string chemStr)
+        {
+            InputStr = chemStr;
+        }
+
+        public void ParseStr()
+        {
+            string elemN = "";
+            for (int i = 0; i < InputStr.Length; i++)
+            {
+                if (allowedSym.Contains(InputStr[i]) == false) // проверка валидности символа
+                    return;
+
+                if (InputStr[i] >= 60 & InputStr[i] <= 90) //блок формирования символа элемента
+                {
+                    elemN = elemN + InputStr[i];
+                    i++;
+                    while (InputStr[i] >= 97 & InputStr[i] <= 122)
+                    {
+                        elemN = elemN + InputStr[i];
+                        i++;
+                    }
+                    //elemQQ el = new elemQQ {elemName = elemN, ChemicalQuantity = new ChemicalQuantity(1, 1)};
+                    ChemList.Add(new ChemicalElement(elemN) , new ChemicalQuantity(1, 1));
+                }
+
+                if (InputStr[i] == '<')//блок проверки индекса
+                {
+                    
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diplom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string chemStr = "H<sub>2</sub>O";
+    
+        }
+    }
+}
