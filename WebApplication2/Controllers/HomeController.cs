e52277349ed7e380fb507429ca4fa53fbc390056
brokenhorn <danila.sergeev2010@yandex.ru>
2022-05-03 13:50:48 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using ChemicalParserValidator;
+
+namespace WebApplication2.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // public ActionResult Index(FormulaProcess)
+        [HttpGet]
+        public ActionResult Index(string str)
+        {
+            FormulaProcess process = new FormulaProcess("", "ad");
+
+            return View(process);
+        }
+        [HttpPost]
+        public ActionResult Index(FormulaProcess formula)
+        {
+            try
+            {
+                ChemicalParser parser = new  ChemicalParser(formula.Str);
+                formula.Answer = parser.GetJsonDescription();
+            }
+            catch (Exception e)
+            {
+                formula.Answer = e.Message;
+            }
+            //formula.Answer = "bbb!";
+            FormulaProcess process = new FormulaProcess(formula.Str, formula.Answer);
+            return View(process);
+        }
+
+    }
+}
---
+++ WebApplication2/Models/FormulaProcess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class FormulaProcess
+    {
+
+        public String Str { set; get; }
+        public String Answer { set; get; }
+        public FormulaProcess(string nStr, string nAnswer)
+        {
+            Str = nStr;
+            Answer = nAnswer;
+        }
+        public FormulaProcess()
+        {
+            Str = "";
+            Answer = "Поле вывода";
+        }
+    }
+}
